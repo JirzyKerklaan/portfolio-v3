96fe90fdf033d63e91fa8e7c37f0b40e878dd2ba
JirzyKerklaan <jirzykerklaan@gmail.com>
2026-03-21 00:57:22 +0100

    :sparkles: Added contact block

---
+++ resources/views/_components/contact.blade.php
@@ -0,0 +1,23 @@
+<div class="contact">
+    <div class="contact-inner">
+        <div class="section-top">
+            <span>/ Contact / P. 005</span>
+        </div>
+        <div class="contact-content">
+            <span>Let's make something legendary.</span>
+            <a href="mailto:jirzykerklaan@gmail.com">jirzykerklaan@gmail.com</a>
+
+            <div class="contact-gradient"></div>
+
+            <div class="contact-links">
+                <a target="_blank" href="https://www.github.com/JirzyKerklaan">Github</a>
+                <a target="_blank" href="https://www.linkedin.com/in/jirzy-kerklaan">LinkedIn</a>
+                <a target="_blank" href="https://www.instagram.com/jirzy_kerklaan">Instagram</a>
+            </div>
+        </div>
+
+        <div class="contact-copyright">
+            <span>&copy; 2026 Jirzy Kerklaan - jirzykerklaan.com</span>
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,7 @@
     @include('_components.manifesto')
     @include('_components.right-now')
     @include('_components.projects')
+    @include('_components.contact')
 
     <div id="custom-cursor">
         <img id="cursor-img" src=""/>
