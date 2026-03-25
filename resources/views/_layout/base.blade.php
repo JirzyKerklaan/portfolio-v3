471f1c494c680ba997dd8c70caf9c8ac26dc6378
JirzyKerklaan <jirzykerklaan@gmail.com>
2026-03-25 15:34:29 +0100

    :lipstick: Mobile design

--- resources/views/_layout/base.blade.php
+++ resources/views/_layout/base.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>@yield('title') | Jirzy Kerklaan</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body data-barba="wrapper">
     <main data-barba="container" data-barba-namespace="@yield('namespace')">
