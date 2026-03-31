1ed00fdab6a630dc0fc238ab6494b96bb6848bce
JirzyKerklaan <jirzykerklaan@gmail.com>
2026-03-31 14:19:22 +0200

    :mag: Updated SEO description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('namespace', 'home')
 
 @section('seo_title', 'Full-Stack Laravel Developer in Vlaardingen, Netherlands')
-@section('seo_description', "Jirzy Kerklaan is a full-stack Laravel developer from Vlaardingen, Netherlands, specializing in PHP and modern web applications. Explore projects and get to know me.")
+@section('seo_description', "Jirzy Kerklaan is a full-stack Laravel developer from Vlaardingen, the Netherlands, specializing in PHP and modern web applications.")
 @section('seo_og_title', 'Full-Stack Laravel Developer in Vlaardingen, Netherlands')
-@section('seo_og_description', "Jirzy Kerklaan is a full-stack Laravel developer from Vlaardingen, Netherlands, specializing in PHP and modern web applications. Explore projects and get to know me.")
+@section('seo_og_description', "Jirzy Kerklaan is a full-stack Laravel developer from Vlaardingen, the Netherlands, specializing in PHP and modern web applications.")
 
 @section('content')
     @include('_components.hero')
