52804fb615238a99c09b82d05e0829c7fc3608a9
JirzyKerklaan <jirzykerklaan@gmail.com>
2026-04-17 14:54:44 +0200

    :sparkles: Finished skills block & database managed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use App\Models\RightNowItem;
+use App\Models\Skill;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
     public function index(): View
     {
+        $skills = Skill::all()->groupBy('column');
+
         $rightNowItems = RightNowItem::all();
         $projects = Project::query()
                         ->where('archived', false)
                         ->orderBy('year', 'desc')
                         ->get();
+
         return view('home', [
+            'skills' => $skills,
             'rightNowItems' => $rightNowItems,
             'projects' => $projects,
         ]);
--- resources/views/_components/skills.blade.php
+++ resources/views/_components/skills.blade.php
@@ -4,97 +4,37 @@
             <span>/ Who am i / P. 003</span>
         </div>
         <div class="skills-content">
-            <div class="skills-content-column">
-                <div class="section-top">
-                    <span>The code</span>
-                </div>
-                <h2>What <br> i build</h2>
+            @foreach($skills as $column => $items)
+                <div class="skills-content-column">
+                    @if ($column === 1)
+                        <div class="section-top">
+                            <span>The code</span>
+                        </div>
+                        <h2>What <br> i build</h2>
+                    @elseif ($column === 2)
+                        <div class="section-top">
+                            <span>The culture</span>
+                        </div>
+                        <h2>What <br> moves me</h2>
+                    @endif
 
-                <div class="skills-list">
-                    <div class="skills-list-item">
-                        <span>Langs</span>
-                        <ul>
-                            <li>PHP</li>
-                            <li>Javascript</li>
-                            <li>Typescript</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>FRONT</span>
-                        <ul>
-                            <li>Vue</li>
-                            <li>Tailwind</li>
-                            <li>SASS</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>BACK</span>
-                        <ul>
-                            <li>Laravel</li>
-                            <li>Express</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>DATA</span>
-                        <ul>
-                            <li>Mysql</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>TOOLS</span>
-                        <ul>
-                            <li>Figma</li>
-                            <li>GSAP</li>
-                            <li>Barba.js</li>
-                        </ul>
+                    <div class="skills-list">
+                        @foreach($items as $item)
+                            <div class="skills-list-item">
+                                <span>{{$item->category}}</span>
+                                <ul>
+                                    @foreach($item->skills as $skill)
+                                        <li>{{$skill}}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
-            </div>
-            <div class="skills-content-line"></div>
-            <div class="skills-content-column">
-                <div class="section-top">
-                    <span>The culture</span>
-                </div>
-                <h2>What <br> moves me</h2>
-
-                <div class="skills-list">
-                    <div class="skills-list-item">
-                        <span>SOUND</span>
-                        <ul>
-                            <li>Bruno Mars</li>
-                            <li>TØP</li>
-                            <li>IPrevail</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>FRONT</span>
-                        <ul>
-                            <li>Vue</li>
-                            <li>Tailwind</li>
-                            <li>SASS</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>BACK</span>
-                        <ul>
-                            <li>Laravel</li>
-                            <li>Express</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>CITY</span>
-                        <ul>
-                            <li>Vlaardingen</li>
-                        </ul>
-                    </div>
-                    <div class="skills-list-item">
-                        <span>NOW</span>
-                        <ul>
-                            <li>Building at Goedemiddag</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+                @if ($column === 1)
+                    <div class="skills-content-line"></div>
+                @endif
+            @endforeach
         </div>
     </div>
 </section>
